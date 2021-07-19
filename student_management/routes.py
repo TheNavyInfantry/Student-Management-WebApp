ae5524a2d49bd687b1c4e1ca7b8c3f9befb20fe3
TheNavyInfantry <nedimcanulusoy@gmail.com>
2021-07-19 14:29:52 +0300

    Update Index page with WTForms and Redirects & Add Flash to Index Page

--- student_management/routes.py
+++ student_management/routes.py
@@ -1,32 +1,32 @@
 from student_management import app
-from flask import render_template, redirect, request
+from flask import render_template, redirect, url_for, request, flash
 from student_management.models import Student
+from student_management.forms import StudentRegisterForm, StudentUpdateForm
 from student_management import db
 
 @app.route("/", methods=['POST', 'GET'])
 def index():
-    # tags = ['ID', 'University Code', 'Name', 'Surname', 'Register Date','Faculty']
-    # tags_of_form = ['university_code','name','surname','register_date','faculty']
 
-    if request.method == 'POST':
-        university_code = request.form['university_code']
-        name = request.form['name']
-        surname = request.form['surname']
-        register_date = request.form['register_date']
-        faculty = request.form['faculty']
-        new_student = Student(university_code=university_code,name=name,
-                              surname=surname,register_date=register_date,faculty=faculty)
+    form = StudentRegisterForm()
+
+    if form.validate_on_submit():
+        new_student = Student(university_code=form.university_code.data, name=form.name.data,
+                              surname=form.surname.data, register_date=form.register_date.data,
+                              faculty=form.faculty.data)
 
         try:
             db.session.add(new_student)
             db.session.commit()
-            return redirect('/')
+            return redirect(url_for('index'))
 
         except:
-            return redirect('/error')
+            return redirect(url_for('error_404'))
 
-    # return render_template('index.html', tags_of_form=tags_of_form, len_tags=len(tags_of_form), tags=tags, len=len(tags))
-    return render_template('index.html')
+    if form.errors != {}: #Means if there are not errors from the validations
+        for error_msg in form.errors.values():
+            flash(f'There was an error: {error_msg}', category='warning')
+
+    return render_template('index.html', form=form)
 
 
 @app.route("/list", methods=['GET'])
@@ -41,10 +41,10 @@ def delete(id):
     try:
         db.session.delete(query_to_delete)
         db.session.commit()
-        return redirect('/list')
+        return redirect(url_for('list_all'))
 
     except:
-        return redirect('/error')
+        return redirect(url_for('error_404'))
 
 @app.route("/update/<int:id>", methods=['GET', 'POST'])
 def update(id):
@@ -59,10 +59,10 @@ def update(id):
 
         try:
             db.session.commit()
-            return redirect('/list')
+            return redirect(url_for('list_all'))
 
         except:
-            return redirect('/error')
+            return redirect(url_for('error_404'))
 
     else:
         return render_template('update.html', update=query_to_update)
