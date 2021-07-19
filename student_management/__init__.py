0256375a9fe5cf97aa7100859ffa4563c782df95
TheNavyInfantry <nedimcanulusoy@gmail.com>
2021-07-19 14:27:55 +0300

    Add Secret Key

--- student_management/__init__.py
+++ student_management/__init__.py
@@ -4,6 +4,7 @@ from datetime import date
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///student.db'
+app.config['SECRET_KEY'] = '52f66d9364665810978000d1'
 db = SQLAlchemy(app)
 
 from student_management import routes
